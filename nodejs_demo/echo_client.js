7733711557b7892743a38def95a2f413c7f5c705
xiaoliuzi <qingpengliu@qq.com>
2015-11-17 22:06:02 +0800

    echo server and client files of nodejs version

---
+++ nodejs_demo/echo_client.js
@@ -0,0 +1,1 @@
+$curl -d 'dhananajay' http://localhost:8080
---
+++ nodejs_demo/echo_server.js
@@ -0,0 +1,17 @@
+var http = require('http');
+ 
+http.createServer(function(request,response){
+ 
+ response.writeHead(200);
+ 
+request.on('data',function(message){
+ 
+ response.write(message);
+ 
+ });
+ 
+ request.on('end',function(){
+ 
+ response.end();
+ });
+ }).listen(8080);
